268bec39fb5a24a6f6bd6e8d0742c391b892e2b3
Muhammad-Rubel <rubel.contact@gmail.com>
2024-10-10 17:16:40 +0600

    Added i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,28 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
+import paraglide from '@inlang/paraglide-astro';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  i18n: {
+    locales: ['en', 'de', 'nl'],
+    defaultLocale: 'en',
+  },
+
+  output: 'server',
+
+  integrations: [
+    tailwind(),
+    svelte(),
+    paraglide({
+      // recommended settings
+      project: './project.inlang',
+      outdir: './src/paraglide', //where your files should be
+    }),
+  ],
+
+  adapter: cloudflare(),
 });
--- src/components/Greetings.svelte
+++ src/components/Greetings.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+  import * as m from '../paraglide/messages';
   let name: string;
 </script>
 
 <div class="my-5 rounded-lg p-5">
+  <p>{m.homepageTitle()}</p>
+
   <h1 class="text-2xl font-semibold mb-4">Hello {name || 'John'}!</h1>
 
   <input
